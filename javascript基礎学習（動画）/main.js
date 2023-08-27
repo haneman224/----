d9f3404a4da0bef89684cef4673ac3993522a21c
haneman224 <ka1an2ta@gmail.com>
2023-08-27 12:11:49 +0900

    test

---
+++ javascript基礎学習（動画）/main.js
@@ -0,0 +1,4 @@
+"use strict";
+let color ="Red" //let＝変数を宣言 colorという変数の定義　let cokorに”red"を代入する。
+//変数にはアルファベット　数字　アンダースコア　ドルマークが使用可能。数字からは使用不可、大文字小文字は区別　予約語も無理
+console.loh(color);// console.logは多分ログだと思う。（定義した変数の名前？）を入れる。
