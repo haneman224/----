ed78e7eb7c85fb8712fe69d7c63e5b2ba9348b71
haneman224 <ka1an2ta@gmail.com>
2023-09-28 06:04:44 +0900

    オブジェクトの理解について。

--- javascript基礎学習（動画）/javascript基礎 HTMLの操作変更/testaaa.js
+++ javascript基礎学習（動画）/javascript基礎 HTMLの操作変更/testaaa.js
@@ -1,6 +1,7 @@
 let taskStatusElement = document.getElementById("taskStatus");
 let toggleButton = document.getElementById("toggleButton");
-
+//箱を作って、その箱に指示を出す、その指示はIDを持つ要素を探して、その要素を作った箱に入れるような指示。
+//まだいまいちわからないのは、書き換えのところ、if文の入力結果を、変えて、出力結果を変えている。なんでか理解できないのはなぜ？
 toggleButton.addEventListener("click", function() {
   let currentStatus = taskStatusElement.textContent;
   if (currentStatus === "未完了") {
